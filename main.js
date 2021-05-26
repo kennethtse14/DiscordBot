c8f9b9210970ad3d2fce840f642a75e40fcb9ef3
kennethtse14 <tse.kenneth79@gmail.com>
2021-05-25 21:44:50 -0400

    created a commands folder to hold different commands

--- main.js
+++ main.js
@@ -2,6 +2,17 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
 
+const fs = require('fs');
+client.commands = new Discord.Collection();
+
+// ensure command files is a javascript file
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
 client.once('ready', () => {
     console.log('Bot is ready!');
 });
@@ -13,7 +24,7 @@ client.on('message', message =>{
     const command = args.shift().toLowerCase()
 
     if(command === 'ping'){
-        message.channel.send('pong!');
+        client.commands.get('ping').execute(message, args);
     }
 });
 
